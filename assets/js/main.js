531c6bcca151416ce89f9fa41dfe60e77e27195d
ArunKum4r <arunvjr107@gmail.com>
2023-09-29 22:05:07 +0530

    removed unwanted logs

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,26 @@ async function getJSONData() {
 }
 
 const searchRecord = async (value) => {
-    console.log('I have got this value!', value.toUpperCase());
-
     const jsonData = await getJSONData();
 
     const data = jsonData.find((record) => {
         return record.code === value.toUpperCase() || value.toUpperCase().startsWith(record.code)
     })
 
-    console.log(data)
-
     const resultSelection = document.querySelector('#resultSection')
 
     if (data) {
         resultSelection.classList.remove('hidden');
+        resultSelection.querySelector("#query").innerText = value.toUpperCase()
+        resultSelection.querySelector("#rto_id").innerText = data.id
+        resultSelection.querySelector("#rto_code").innerText = data.code
+        resultSelection.querySelector("#rto_loc").innerText = data.location
+        resultSelection.querySelector("#type").innerText = data.type
+        resultSelection.querySelector("#district").innerText = data.district
     } else {
         resultSelection.classList.add('hidden')
     }
-    resultSelection.querySelector("#query").innerText = value.toUpperCase()
-    resultSelection.querySelector("#rto_id").innerText = data.id
-    resultSelection.querySelector("#rto_code").innerText = data.code
-    resultSelection.querySelector("#rto_loc").innerText = data.location
-    resultSelection.querySelector("#type").innerText = data.type
-    resultSelection.querySelector("#district").innerText = data.district
-    console.log(resultSelection)
+
 };
 
 inputEl.addEventListener('keyup', (e) => {
